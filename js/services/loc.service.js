466817f15c3772efacb26858015dfea75dc3f2c1
Miriam Baranovska <maryam1649@gmail.com>
2021-04-27 17:46:41 +0300

    added locations key in location service

--- js/services/loc.service.js
+++ js/services/loc.service.js
@@ -2,16 +2,19 @@ import { storageService } from './storage.service.js'
 export const locService = {
     getLocs
 }
-var locs = storageService.loadFromStorage() ||  [
+
+const LOCATIONS_KEY = 'locations';
+
+var locs = storageService.loadFromStorage(LOCATIONS_KEY) ||  [
     { name: 'Loc1', lat: 32.047104, lng: 34.832384 },
     { name: 'Loc2', lat: 32.047201, lng: 34.832581 }
-]
+];
 
 function getLocs() {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
             resolve(locs);
-        }, 2000)
+        }, 2000);
     });
 }
 
