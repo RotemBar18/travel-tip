abd375fa2e886e9d9d4a95e285caba0df0b753aa
Rotem Bar <rotem68907@gmail.com>
2021-04-27 19:02:24 +0300

    pushi

--- js/services/loc.service.js
+++ js/services/loc.service.js
@@ -1,10 +1,11 @@
 import { storageService } from './storage.service.js'
 export const locService = {
-    getLocs
+    getLocs,
+    removeLoc
 }
-var locs = storageService.loadFromStorage() ||  [
-    { name: 'Loc1', lat: 32.047104, lng: 34.832384 },
-    { name: 'Loc2', lat: 32.047201, lng: 34.832581 }
+var locs = storageService.loadFromStorage() || [
+    { id: 'aaa', name: 'Loc1', lat: 33.047104, lng: 34.832384 },
+    { id: 'bbb', name: 'Loc2', lat: 32.047201, lng: 34.832581 }
 ]
 
 function getLocs() {
@@ -13,6 +14,15 @@ function getLocs() {
             resolve(locs);
         }, 2000)
     });
+}
+
+function removeLoc(locId) {
+    let currLocIdx = locs.findIndex(loc => {
+        return loc.id === locId
+    })
+    locs.splice(currLocIdx,1)
+    // storageService.saveToStorage(LOCATIONS_KEY,locs)
+
 }
 
 
