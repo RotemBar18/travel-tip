8050b72db5bb9c7abc3904a9210d69670f81aff4
Rotem Bar <rotem68907@gmail.com>
2021-04-27 19:47:48 +0300

    fff

--- js/services/loc.service.js
+++ js/services/loc.service.js
@@ -1,51 +1,49 @@
-import {
-    storageService
-} from './storage.service.js';
-import {
-    utilService
-} from './util.service.js';
+import { storageService } from './storage.service.js';
+import { utilService } from './util.service.js';
 export const locService = {
     getLocs,
     removeLoc,
     addLoc
 }
+
 const LOCATIONS_KEY = 'locations';
 
-var locs = storageService.loadFromStorage(LOCATIONS_KEY) || [
-    { id: 'aaa', name: 'Loc1', lat: 33.047104, lng: 34.832384 },
-    { id: 'bbb', name: 'Loc2', lat: 32.047201, lng: 34.832581 }
+var gLocs = storageService.loadFromStorage(LOCATIONS_KEY) || [
+    { id: 'aaa', name: 'yam', lat: 33.047104, lng: 34.832384 },
+    { id: 'bbb', name: 'ramat gan', lat: 32.047201, lng: 34.832581 }
 ]
 
 function getLocs() {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
-            resolve(locs);
+            resolve(gLocs);
         }, 500);
     });
 }
 
 function removeLoc(locId) {
-    let currLocIdx = locs.findIndex(loc => {
+    let currLocIdx = gLocs.findIndex(loc => {
         return loc.id === locId
     })
-    locs.splice(currLocIdx, 1)
-    storageService.saveToStorage(LOCATIONS_KEY,locs)
+    gLocs.splice(currLocIdx, 1)
+    storageService.saveToStorage(LOCATIONS_KEY, gLocs)
 
 }
 
-function addLoc(name, lat, lng) {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            var newLoc = {
-                'id': utilService.makeId(),
-                'name': name,
-                'lat': lat,
-                'lng': lng
-            };
-            locs.push(newLoc);
-            storageService.saveToStorage(LOCATIONS_KEY, locs);
-            resolve(locs);
-        }, 500);
-    });
+function addLoc(lat, lng) {
+    const newLoc = {
+        id: utilService.makeId(),
+        lat,
+        lng
+    };
+    getLocationName(lat, lng)
+        .then(res => newLoc.name = res)
+    gLocs.push(newLoc);
+    storageService.saveToStorage(LOCATIONS_KEY, gLocs);
+
+}
+
+function getLocationName(lat,lng){
+
 }
 
