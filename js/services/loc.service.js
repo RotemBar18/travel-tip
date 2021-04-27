671d04a0f82d6e29fa819ce576ab6c1c3b8192d0
Miriam Baranovska <maryam1649@gmail.com>
2021-04-27 18:02:04 +0300

    adding to location service

--- js/services/loc.service.js
+++ js/services/loc.service.js
@@ -1,21 +1,52 @@
-import { storageService } from './storage.service.js'
+import {
+    storageService
+} from './storage.service.js'
 export const locService = {
     getLocs
 }
 
 const LOCATIONS_KEY = 'locations';
 
-var locs = storageService.loadFromStorage(LOCATIONS_KEY) ||  [
-    { name: 'Loc1', lat: 32.047104, lng: 34.832384 },
-    { name: 'Loc2', lat: 32.047201, lng: 34.832581 }
+var locs = storageService.loadFromStorage(LOCATIONS_KEY) || [{
+        name: 'Loc1',
+        lat: 32.047104,
+        lng: 34.832384
+    },
+    {
+        name: 'Loc2',
+        lat: 32.047201,
+        lng: 34.832581
+    }
 ];
 
 function getLocs() {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
             resolve(locs);
-        }, 2000);
+        }, 500);
     });
 }
 
+function addLoc(name, lat, lng) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            locs.push({
+                'name': name,
+                'lat': lat,
+                'lng': lng
+            });
+            storageService.saveToStorage(LOCATIONS_KEY, locs);
+            resolve(locs);
+        }, 500);
+    });
+}
 
+function removeLoc(index) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            locs.splice(index, 1);
+            storageService.saveToStorage(LOCATIONS_KEY, locs);
+            resolve(locs);
+        }, 500);
+    });
+}
